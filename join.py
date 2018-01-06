3cc74346d16b5e96b56ade0f36b35df92925e960
tbovee <tbovee@gmail.com>
2018-01-06 15:11:53 -0800

    Join 2 CSV files with a common key

---
+++ join.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+# A learning project to learn python, just for me.
+'''
+Program join imports two files 
+	in the csv format (no quotes around strings) 
+	with a single key column
+It reads file "Fleft", which will typically be the smallest table,
+and then selects each matching record from file "Fright'.
+It will combine all of the fields in Fleft with all of the fields, excepting the key field,
+from Fright.
+'''
+
+# Import modules
+import sys
+# import pandas as pd
+
+def importFile() :
+  Fleft = open ('Tableleft.txt', 'rU')
+  for line in Fleft :
+  	# Split into fields
+  	# Save the number of fields
+  	
+def scanFright) :
+  
+
+def main() : 
+  print "Howdy, Pardner!"
+  
+  
+
+# CALL MAIN
+
+if __name__ == '__main__' :
+  main()
+
+# --EOF--
