c86d4591ae26cdcc3b5243797a746f3e0e22145c
tbovee <tbovee@gmail.com>
2018-01-06 15:52:59 -0800

    join.py
    Primary code

--- join.py
+++ join.py
@@ -9,6 +9,28 @@ It reads file "Fleft", which will typically be the smallest table,
 and then selects each matching record from file "Fright'.
 It will combine all of the fields in Fleft with all of the fields, excepting the key field,
 from Fright.
+
+Needed vVariables
+
+tableleft string
+tableright string
+keyleft integer
+keyright integer
+delimleft integer
+delimright integer
+colnames boolean
+
+Commandline 
+join [parameters]
+parameters =
+-tl tableleft
+-tr tableright
+-kl keyleft
+-kr keyright
+-dl delimleft
+-dr delimright
+-n  colnames
+
 '''
 
 # Import modules
@@ -16,10 +38,14 @@ import sys
 # import pandas as pd
 
 def importFile() :
-  Fleft = open ('Tableleft.txt', 'rU')
+  Fleft = open (tableleft', 'rU')
   for line in Fleft :
   	# Split into fields
+  	### How do I show curr to be an array
+  	curr = line.split(delimleft)
+  	### check use of len
   	# Save the number of fields
+  	k = len(curr)
   	
 def scanFright) :
   
