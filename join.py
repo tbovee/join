7d1cbe828cde1af38877205f85d41b203d201f0f
tbovee <tbovee@gmail.com>
2018-01-10 10:21:19 -0800

    join.py

--- join.py
+++ join.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 # project as part of my learning python, restricted to the built-ins
-# Version 20180109.1545P
-# STATUS: Syntax error issues
+# Version 20180110.xxxxA
+# STATUS: 
 '''
 PURPOSE
 :
@@ -41,9 +41,9 @@ Read the command line arguments.
 Load file1 and file2 into separate lists.
 	For each file,
 	split each line and extract the key data; increment the tablecount to create the
-	ptr data; add each their ptr lists.
-	While loading extact the key field data and the line number into a ptr list
-	Ptr list structure: keydata, pointer, keydata, pointer...
+	pointer data; add each their pointers lists.
+	While loading extact the key field data and the line number into a pointers list
+	Pointers list structure: keydata, pointer, keydata, pointer...
 		where the pointer is associated with the preceding keydata.
 
 Joining the files
@@ -53,12 +53,12 @@ If tablecount1 is larger than tablecount2, then assign
 tablecount1 to smalltable; otherwise, assign it to bigtable. Assign
 the unassigned numbered table to the yet unassigned bigtable or smalltable.
 
-Assign the associated pointer lists in the same way, ptr1 and ptr2 to bigptr and 
-smallptr.
+Assign the associated pointer lists in the same way, pointers1 and pointers2 to bigpointers and 
+smallpointers.
 
 Each table has a separate function.
 
-The ptr list for the largest, bigtable, is read one line at a time. 
+The pointers list for the largest, bigtable, is read one line at a time. 
 For each key it contains, it loops through the pointers of the 
 other table, smalltable, until it finds a matching key.
 When a matching key is found, the two lines are combined and written to
@@ -70,8 +70,8 @@ The field number holding the keys is called ...key...
 The character delimiting fields in a CSV file is called ...sep...
 The presence or absense of fieldnames in the first line is denoted by ...names...
 A list containing data is called ...table...
-A list containing pointers to the data is called ...ptrs...
-A number pointing to a single record in a table is a a ...ptr...
+A list containing pointers to the data is called ...pointers...
+A number pointing to a single record in a table is a ...pointer...
 The total number of records in a table is called ...count...
 
 
@@ -92,15 +92,15 @@ sep2 = ","		# char sets the field seprator character in file2 to default comma
 names1 = False	# bool if true, designates the first line of file1 to be fieldnames
 names2 = False	# bool if true, designates the first line of file2 to be fieldnames
 
-tablecount1 = 0	# int holds total number of non-fieldname lines in file1
-tablecount2 = 0 # int holds total number of non-fieldname lines in file2
+tablecount1 = 0		# int holds total number of non-fieldname lines in file1
+tablecount2 = 0 	# int holds total number of non-fieldname lines in file2
 
-bigtable = ""	# list holds the data with the greater number of records
-smalltable = ""	# list holds the data with the smaller number of records
-bigptrs = ""	# list holds the key data and ptr for each line in bigtable
-smallptrs = ""	# list holds the key data and ptr for each line in smalltable.
-bigptr = ""		# int points to current record in bigable
-smallptr = ""	# int points to current record in smalltable
+bigtable = ""		# list holds the data with the greater number of records
+smalltable = ""		# list holds the data with the smaller number of records
+bigpointers = ""	# list holds the key data and pointer for each line in bigtable
+smallpointers = ""	# list holds the key data and pointer for each line in smalltable.
+bigpointer = ""		# int points to current record in bigable
+smallpointer = ""	# int points to current record in smalltable
 
 # END Global names
 
@@ -110,82 +110,81 @@ import sys
 # END Import modules
 
 
+def arguments():
 # Get arguments
-n = len(sys.argv)-1
-# Error checking here if n < 2
-file1 = sys.argv[0]
-file2 = sys.argv[1]
-outfile = sys.argv[2]
-for i in range(2,n):
-	if sys.argv[i] == '-kl':
-		key1 = sys.argv[i+1]
-	elif sys.argv[i] == '-k2':
-		key2 = sys.argv[i+1]
-	elif sys.argv[i] == '-s1':
-		sep1 = sys.argv[i+1]
-	elif sys.argv[i] == '-s2':
-		sep2 = sys.argv[i+1]
-	elif sys.argv[i] == '-n1':
-		names1 = TRUE
-	elif sys.argv[i] == '-n2':
-		names2 = TRUE
-	else:
-		print "Usage: join.py input-filename1 input-filename2 -output-filename [-type] [value] ..."
+	n = len(sys.argv)-1
+	# Error checking here if n < 2
+	file1 = sys.argv[0]
+	file2 = sys.argv[1]
+	outfile = sys.argv[2]
+	for i in range(2,n):
+		if sys.argv[i] == '-kl':
+			key1 = sys.argv[i+1]
+		elif sys.argv[i] == '-k2':
+			key2 = sys.argv[i+1]
+		elif sys.argv[i] == '-s1':
+			sep1 = sys.argv[i+1]
+		elif sys.argv[i] == '-s2':
+			sep2 = sys.argv[i+1]
+		elif sys.argv[i] == '-n1':
+			names1 = TRUE
+		elif sys.argv[i] == '-n2':
+			names2 = TRUE
 #END Get arguments()
 
-def procline(s,sep,key,table,ptrs,ptr)
+def procline(s,sep,key,table,pointers,pointer):
 # Splits s into fields and inserts into appropriate lists
-'''
-Is the var tablecount needed here?
-'''
+	'''
+	Is the var tablecount needed here?
+	'''
   	curr = s.split(sep)
   	k = len(curr)
 	if key < k:
 		table.append(curr)
-		ptrs.append(curr[key])
-		ptrs.append(ptr)
+		pointers.append(curr[key])
+		pointers.append(pointer)
 # END procline
 
 
-def importfile(sep,key,table,ptrs,ptr,file,names,tablecount)
+def importfile(sep,key,table,pointers,pointer,file,names,tablecount):
 # Reads file from disk and passes each line to procfile()
-'''
-Q: Would it be more efficient to read the entire file into a list and then get the size
-from the list rather than doing a count? Probably yes.
-'''
-	ptr = 0
+	'''
+	Q: Would it be more efficient to read the entire file into a list and then get the size
+	from the list rather than doing a count? Probably yes.
+	'''
+	pointer = 0
 	F = open(file, 'r')
 	for line in F :
-		ptr = ptr + 1
-		if ptr == 1
-			if names == FALSE
-				procline(line,sep,key,table,ptrs)  
-			else
-				procline(line,sep,key,table,ptrs)  
-	tablecount = ptr
+		pointer = pointer + 1
+		if pointer == 1:
+			if names == FALSE:
+				procline(line,sep,key,table,pointers)  
+			else:
+				procline(line,sep,key,table,pointers)  
+	tablecount = pointer
 # END importfile
 
-def importfiles() :
+def importfiles():
 # Imports the input files in turn, assesses the size of each, and assigns
 # the larger to the big... series of lists and the smaller to the small...
 # series of lists
-	count1 = importfile(sep1,key1,table1,ptrs1,ptr1,file1,names1,tablecount1)
-	count2 = importfile(sep2,key2,table2,ptrs2,ptr2,file2,names2,tablecount2)
+	count1 = importfile(sep1,key1,table1,pointers1,pointer1,file1,names1,tablecount1)
+	count2 = importfile(sep2,key2,table2,pointers2,pointer2,file2,names2,tablecount2)
 	if count1 > count2:
 		bigfile = file1
 		bigkey = key1
 		smallfile = file2
 		smallkey = key2
-	else
+	else:
 		bigfile = file2
 		bigkey = key2
 		smallfile = file1
 		smallkey = key1
-			'''
-			Q: Does the following zeroing out save memory?
-			In making the assignments above, do the assigned files take on a separate existence
-			from their destination variables?
-			'''
+		'''
+		Q: Does the following zeroing out save memory?
+		In making the assignments above, do the assigned files take on a separate existence
+		from their destination variables?
+		'''
 	file1 = ""
 	key1 = ""
 	file2 = ""
